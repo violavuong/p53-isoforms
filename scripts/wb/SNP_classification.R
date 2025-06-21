61f7f7dfcd4b19efb1f03feef05578b4f7f51474
unknown <viola.vuong@gmail.com>
2025-06-21 16:03:36 +0200

    update

---
+++ scripts/wb/SNP_classification.R
@@ -0,0 +1,45 @@
+#!/usr/bin/r
+
+## file: SNP_classification
+## last update: 20-06-2025
+
+# ---- Main ----
+library(data.table)
+library(gtools)
+library(readxl)
+library(purrr)
+library(tidyverse)
+
+
+# global input
+setwd("C:/Users/Dell/Alma Mater Studiorum Università di Bologna/PROJECT_TP53-isoforms - Documents/")
+
+snp_db <- fread("data/original_db/final_dataset_cna.csv")[, -1]
+snp_meta <- read_excel("data/original_db/Tabella pz TP53 (5).xlsx", sheet = "SNP")
+
+
+
+# ---- data manipulation ----
+#homogenization of SNP ID
+snp_meta <- snp_meta %>% mutate(N_SNP = str_replace(SNP_numero, " ", "_"))
+SNPs <- na.omit(snp_meta$N_SNP)
+
+#filtering and arranging by chrarm and SNP ID
+wb_snp_tmp_df <- snp_db %>% 
+  filter(N_SNP %in% SNPs) %>%
+  select(MPC = Experiment_MPC_SNP.MPC, DNA, GITC, N_SNP, SNP, chrarm, weighted_mean_CN)
+wb_snp_tmp_df <- wb_snp_tmp_df[mixedorder(wb_snp_tmp_df$chrarm), ]
+wb_snp_tmp_df <- wb_snp_tmp_df %>% arrange(N_SNP)
+
+
+# ---- classifying by selected cut-off ----
+cut_offs <- c(class_10 = 0.10, class_20 = 0.20, class_50 = 0.50, class_80 = 0.80)
+wb_snp_tmp_df <- wb_snp_tmp_df %>% mutate(abs_CN_diff = abs(weighted_mean_CN - 2))
+
+cut_offs_cols <- map2_dfc(.x = names(cut_offs), 
+                          .y = as.list(cut_offs), 
+                          .f = ~ {tibble(!!sym(.x) := if_else(wb_snp_tmp_df$abs_CN_diff > .y, 1, 0))})
+
+wb_snp_df <- cbind(wb_snp_tmp_df, cut_offs_cols)
+
+write_tsv(wb_snp_df, paste0("data/wb_snp_df.txt"))
