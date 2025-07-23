ae1138ded0928683d03e549d92172ad26da1a06c
unknown <viola.vuong@gmail.com>
2025-07-23 17:32:05 +0200

    update

--- scripts/mmrf/transcript_based/estimation.R
+++ scripts/mmrf/transcript_based/estimation.R
@@ -1,10 +1,11 @@
 #!/usr/bin/r
 
 ## file: estimation.R
-## last update: 22-07-2025
+## last update: 23-07-2025
 
 
 library(data.table)
+library(ggplot2)
 library(readxl)
 library(tidyverse)
 
@@ -20,9 +21,9 @@ mmrf_counts <- fread(paste0(filePath, "MMRF_CoMMpass_IA22_salmon_transcriptUnstr
   rename_with(~"transcript", "Transcript")
 mmrf_cln_per_pt <- fread(paste0("clinical_data/mmrf_cln_per_pt.txt"))
 
-mmrf_tp53_tetra <- read_excel("mmrf_tp53.xlsx", sheet = "isoforms") %>% 
-  filter(NAME %in% c("p53a", "D40p53a", "D133p53a"))
-IDs <- mmrf_tp53_tetra$`ENSEMBL mRNA`
+IDs <- read_excel("mmrf_tp53.xlsx", sheet = "isoforms") %>% 
+  filter(NAME %in% c("p53a", "p53b", "D40p53a", "D133p53a")) %>%
+  pull(`ENSEMBL mRNA`)
 
 # filtering counts 
 mmrf_counts_per_pt <- filterMMRF(mmrf_counts, mmrf_cln_per_pt) %>%
@@ -30,7 +31,7 @@ mmrf_counts_per_pt <- filterMMRF(mmrf_counts, mmrf_cln_per_pt) %>%
 
 mmrf_tp53_counts_per_pt <- as.data.frame(t(mmrf_counts_per_pt %>%
   filter(transcript %in% IDs)  %>%
-  mutate(transcript = c("p53_FL", "Δ40p53α", "Δ133p53α")) %>%
+  mutate(transcript = c("p53_FL", "p53β", "Δ40p53α", "Δ133p53α")) %>%
   column_to_rownames("transcript"))) 
 
 write_tsv(mmrf_tp53_counts_per_pt, "transcript_based/mmrf_tp53_counts_per_pt.txt")
@@ -41,7 +42,9 @@ write_tsv(mmrf_tp53_counts_per_pt, "transcript_based/mmrf_tp53_counts_per_pt.txt
 mmrf_tp53_counts_per_pt <- mmrf_tp53_counts_per_pt + 1 #need to check if it makes sense
 
 computeProb <- function(r, n){
-  return(r^abs(n-4) / ((1 + r) ^ n * (1 + r) ^ (4 - n)))
+  num <- r^abs(n - 4)
+  den <- ((1 + r) ^ n) * ((1 + r) ^ (4 - n))
+  return(num/den)
 }
 
 roundSig <- function(x, digits = 3) {
@@ -51,15 +54,52 @@ roundSig <- function(x, digits = 3) {
 mmrf_tp53_ratio_per_pt <- mmrf_tp53_counts_per_pt %>%
   mutate(r_Δ40_FL = roundSig(Δ40p53α/p53_FL), 
          r_Δ133_FL = roundSig(Δ133p53α/p53_FL),
+         P4_FL_Δ40 = roundSig(computeProb(p53_FL, 4)),
          P3_FL_Δ40 = roundSig(4 * computeProb(r_Δ40_FL, 3)), 
          P2_FL_Δ40 = roundSig(6 * computeProb(r_Δ40_FL, 2)), 
          P1_FL_Δ40 = roundSig(4 * computeProb(r_Δ40_FL, 1)), 
          P0_FL_Δ40 = roundSig(computeProb(r_Δ40_FL, 0)), 
+         P4_FL_Δ133 = roundSig(computeProb(p53_FL, 4)),
          P3_FL_Δ133 = roundSig(4 * computeProb(r_Δ133_FL, 3)), 
          P2_FL_Δ133 = roundSig(6 * computeProb(r_Δ133_FL, 2)), 
          P1_FL_Δ133 = roundSig(4 * computeProb(r_Δ133_FL, 1)), 
-         P0_FL_Δ133 = roundSig(computeProb(r_Δ133_FL, 0))) %>%
+         P0_FL_Δ133 = roundSig(computeProb(r_Δ133_FL, 0)), 
+         r_p53β_FL = roundSig(p53β/p53_FL), 
+         r_p53β_Δ40 = roundSig(p53β/Δ40p53α), 
+         r_p53β_Δ133 = roundSig(p53β/Δ133p53α)) %>%
   rownames_to_column("PUBLIC_ID")
 
 write_tsv(mmrf_tp53_ratio_per_pt, "transcript_based/mmrf_tp53_ratio_per_pt.txt")
+
+
+# ---- Plotting ----
+#D40
+prob_Δ40_FL <- mmrf_tp53_ratio_per_pt %>% select(PUBLIC_ID, P4_FL_Δ40, P3_FL_Δ40, P2_FL_Δ40, P1_FL_Δ40, P0_FL_Δ40)
+stats <- unlist(lapply(seq_along(prob_Δ40_FL)[-1], function(x) quantile(prob_Δ40_FL[, x], probs = c(0.25, 0.50, 0.75))))
+Δ40_FL_stats <- data.frame(group = colnames(prob_Δ40_FL)[-1],
+                           IQR1 = stats[c(1,4,7,10,13)],
+                           median = stats[c(2,5,8,11,14)], 
+                           IQR3 = stats[c(3,6,9,12,15)])
+
+prob_Δ40_FL_long <- pivot_longer(prob_Δ40_FL, cols = starts_with(c("P4", "P3", "P2", "P1", "P0")), names_to = "group", values_to = "probability") %>%
+  left_join(Δ40_FL_stats, by = "group")
+prob_Δ40_FL_long$group <- factor(prob_Δ40_FL_long$group, levels = unique(prob_Δ40_FL_long$group))
+
+prob_Δ40_FL_long %>%
+  ggplot(aes(x = group, y = probability)) +
+    geom_point(aes(colour = group)) +
+    #geom_errorbar(aes(ymin = IQR1, ymax = IQR3)) +
+    stat_summary(fun.data = prob_Δ40_FL_long, fun.args = list(IQR1, IQR3), geom = "errorbar", color = "black", width = 0.4) +
+    stat_summary(fun.y = median, geom = "point", color = "black") +
+    theme_minimal()
+
+#D133
+prob_Δ133_FL <- mmrf_tp53_ratio_per_pt %>%
+  select(PUBLIC_ID, P4_FL_Δ133, P3_FL_Δ133, P2_FL_Δ133, P1_FL_Δ133, P0_FL_Δ133) %>%
+  pivot_longer(cols = starts_with(c("P4", "P3", "P2", "P1", "P0")), names_to = "group", values_to = "probability")
+prob_Δ133_FL$group <- factor(prob_Δ133_FL$group, levels = unique(prob_Δ133_FL$group))
+
+prob_Δ133_FL %>%
+  ggplot(aes(x = group, y = probability)) +
+  geom_boxplot()
 
