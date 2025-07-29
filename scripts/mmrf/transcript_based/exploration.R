6ea1b7532af7f422a95d88ae5fc2c5f543bf53dc
unknown <viola.vuong@gmail.com>
2025-07-29 16:29:48 +0200

    update

--- scripts/mmrf/transcript_based/exploration.R
+++ scripts/mmrf/transcript_based/exploration.R
@@ -78,8 +78,6 @@ sink()
 
 
 # ---- categorization ----
-#mmrf_tp53_cpm_per_pt[mmrf_tp53_cpm_per_pt==0] <- NA
-#mmrf_tp53_cpm_per_pt[is.na(mmrf_tp53_cpm_per_pt)] <- 0
 median_per_tp53 <- mmrf_tp53_cpm_per_pt %>% summarise(across(everything(), ~median(.[. > 0])))
 
 mmrf_tmp_tp53_per_pt <- mmrf_tp53_cpm_per_pt %>%
