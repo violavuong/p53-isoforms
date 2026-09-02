4dc7294a5e020a8851ae645a666dbd973009dda0
Viola Meixian Vuong <violameixian.vuong2@unibo.it>
2026-09-02 11:24:14 +0200

    recreated dataset to solve a bug

--- scripts/mmrf/IA24/mmrf_integration.R
+++ scripts/mmrf/IA24/mmrf_integration.R
@@ -3,7 +3,7 @@
 
 # file: mmrf_integration
 # aim: merge genomic and transcriptomic data based on the key "public_id"
-# last update: 01-09-26
+# last update: 02-09-26
 
 
 library(data.table)
@@ -43,8 +43,10 @@ mmrf_per_pt <- read.xlsx("mmrf_omics_per_pt.xlsx")
 mmrf_surv <- read.xlsx("commpass_surv.xlsx")
 
 mmrf_biochem <- read.xlsx("C:/Users/violameixian.vuong2/Alma Mater Studiorum Università di Bologna/MM group - Vuong_Viola_Meixian/ims_analysis/old_mmrf_IA24/db_analisi/clinical/mmrf_biochem_per_pt_030626.xlsx")
+
 mmrf_labs <- fread("C:/Users/violameixian.vuong2/Alma Mater Studiorum Università di Bologna/Bioinformatics Seràgnoli - IA24/clinical_data/labs_deid.csv")
+
 mmrf_complete_per_pt <- left_join(mmrf_per_pt, mmrf_surv, by = "public_id") %>%
   left_join(mmrf_biochem, by = "public_id")
 
-write.xlsx(mmrf_complete_per_pt, "mmrf_complete_data_per_pt.xlsx")
+write.xlsx(mmrf_complete_per_pt, "mmrf_complete_3_data_per_pt.xlsx")
--- scripts/mmrf/IA24/transcriptomics/2_mmrf_cohort_definition.R
+++ scripts/mmrf/IA24/transcriptomics/2_mmrf_cohort_definition.R
@@ -3,7 +3,7 @@
 
 # file: 2_mmrf_cohort_definition
 # aim: filtering the original dataset for first line pts expressing selected p53 isoforms
-# last update: 12-08-26
+# last update: 02-09-26
 
 
 library(data.table)
@@ -17,7 +17,7 @@ mmrfDir <- "C:/Users/violameixian.vuong2/Alma Mater Studiorum Università di Bol
 
 
 # ---- Define cohort ----
-mmrf_p53_tpm_per_pt <- mmrf_tpm_per_pt %>% #fread("mmrf_tpm_per_pt_ia24.txt") %>%
+mmrf_p53_tpm_per_pt <- fread("complete_db/mmrf_tpm_per_pt_ia24.txt") %>%
   filter(line==1, specimen=="BM") %>%
   select(public_id, p53_FL = ENST00000269305, p53_beta = ENST00000420246, p53_gamma = ENST00000455263, delta40_p53_alpha = ENST00000610292, 
          delta133_p53_alpha = ENST00000504937, delta133_p53_beta = ENST00000510385, delta133_p53_gamma = ENST00000504290)
@@ -34,7 +34,7 @@ median_per_isoform <- mmrf_p53_tpm_per_pt %>%
 
 mmrf_p53_expression_cat <- mmrf_p53_presence_cat %>%
   mutate(across(c(2:8), ~ifelse(.==0, "absent", ifelse(.x > median_per_isoform[[cur_column()]], "high", "low")), .names = "{col}_exp")) %>%
-  select(public_id, starts_with("p53_FL"), starts_with("p53_beta"), starts_with("p53_gamma"), starts_with("delta_40"), 
+  select(public_id, starts_with("p53_FL"), starts_with("p53_beta"), starts_with("p53_gamma"), starts_with("delta40"), 
          starts_with("delta133_p53_alpha"), starts_with("delta133_p53_beta"), starts_with("delta133_p53_gamma")) %>%
   arrange(public_id)
 
